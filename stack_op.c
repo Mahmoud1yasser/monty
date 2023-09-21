d650c832c706539143f3d50f3f546bce512a9668
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-09-21 11:54:03 -0700

    abdo

--- stack_op.c
+++ stack_op.c
@@ -1,33 +1,39 @@
 #include "monty.h"
 
 /**
- * mul_nodes - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
- */
+ * mul_nodes - Adds the top two elemes
+ *
+ * @stack: Pointer to a pointer
+ *
+ * @line_number: Interger
+*/
+
 void mul_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	int s;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 		more_err(8, line_number, "mul");
 
 	(*stack) = (*stack)->next;
-	sum = (*stack)->n * (*stack)->prev->n;
-	(*stack)->n = sum;
+	s = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = s;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
 
 
 /**
- * mod_nodes - Adds the top two elements of the stack.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @line_number: Interger representing the line number of of the opcode.
- */
+ * mod_nodes - Adds the top two elements
+ *
+ * @stack: Pointer to a pointer
+ *
+ * @line_number: Interger
+*/
+
 void mod_nodes(stack_t **stack, unsigned int line_number)
 {
-	int sum;
+	int s;
 
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 
@@ -37,8 +43,8 @@ void mod_nodes(stack_t **stack, unsigned int line_number)
 	if ((*stack)->n == 0)
 		more_err(9, line_number);
 	(*stack) = (*stack)->next;
-	sum = (*stack)->n % (*stack)->prev->n;
-	(*stack)->n = sum;
+	s = (*stack)->n % (*stack)->prev->n;
+	(*stack)->n = s;
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
